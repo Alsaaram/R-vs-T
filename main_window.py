505c8c4ecbed7b123a0fc059471ce7a4b72acdee
Alsaaram <131556435+Alsaaram@users.noreply.github.com>
2024-08-25 18:18:51 -0400

    Window resizing

--- main_window.py
+++ main_window.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import sys
-
+# Look into plotly or mathlibchart for charts and graphs
 from PyQt5.QtWidgets import QWidget
 
 class MainWindow(QMainWindow):
@@ -11,8 +11,9 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         
-        self.setWindowTitle("R Vs T")            # Sets the name of the window
-        self.setFixedSize(QSize(800, 600))       # Sets the FIXED size of the window (Length, Width)
+        self.setWindowTitle("R Vs T")               # Sets the name of the window
+        self.setMinimumSize(QSize(800, 600))        # Sets the Minimum size of the window
+        self.setMaximumSize(QSize(1200, 1000))      # Sets the FIXED size of the window (Length, Width)
         
         label = QLabel("Testing")
         label.setAlignment(Qt.AlignCenter)
